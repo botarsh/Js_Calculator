de5db4307c472bf990a91e39ab72155b62c0265c
jasminebanger <37240370+jasminebanger@users.noreply.github.com>
2019-11-24 19:33:20 +0530

    Update cal.js

--- calculator/cal.js
+++ calculator/cal.js
@@ -1,18 +1,19 @@
+// script for input section 
 function insert(num){
     document.form.textview.value =document.form.textview.value+num;
 }
-
+// script for equal button
 function equal(){
     var exp=document.form.textview.value;
     if(exp){
         document.form.textview.value=eval(exp);
     }
 }
-
+// script for clear button
 function clean(){
     document.form.textview.value="";
 }
-
+// script for back button
 function back(){
     var exp=document.form.textview.value;
     document.form.textview.value=exp.substring(0,exp.length-1);
